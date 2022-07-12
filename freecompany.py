1f6c3af247c8ee36c60f73f56c32860af5f9a0da
johan-sorman <johan.sorman@gmail.com>
2022-07-12 20:26:34 +0200

    Updating naming and added comments

--- freecompany.py
+++ freecompany.py
@@ -2,32 +2,49 @@ import requests, re
 from bs4  import BeautifulSoup
 import datetime
 
-# The base url to the Free Company page on Lodestone
+        # The base url to the Free Company page on Lodestone
+        
 url = "https://eu.finalfantasyxiv.com/lodestone/freecompany/"
 
-# ID for the Free Company we want to scrape/parse
+        # ID for the Free Company we want to scape
+        
 id = "9279667032196907056"
 
-# Grabs html code from Lodestone
+        # Grabs html code from Lodestone
 
 result = requests.get(url+id)
 
-doc = BeautifulSoup(result.text, "html.parser")
-lst2 = doc.find_all("p", {"class": "freecompany__text__name"}) 
+lodestone_page = BeautifulSoup(result.text, "html.parser")
 
-l = str(lst2)
-fcName = re.findall(r"(?<=>)[^<]+",l)
+        # Contains data for Member Count and Current Rank
+
+lst = lodestone_page.find_all("p", {"class": "freecompany__text"}) 
+
+        # Contains data for Name, Tag & Formed Date
+lst2 = lodestone_page.find_all("p", {"class": "freecompany__text__name"}) 
+
+for Rank in lst[4]:
+    fcRank = Rank
+for Members in lst[3]:
+    activeMembers = Members
+LStr = str(lst2)
+fcName = re.findall(r"(?<=>)[^<]+",LStr)
 
 timestamp = fcName[9]
 formed = re.findall(r"\b\d+\b", timestamp)
 for formed in formed:
     formedDate = formed
+    formedDate = int(formedDate) # Convert to INT in order for datetime function to do it's thing
 
-    formedDate = int(formedDate)
+
+
+
+        # Class so functions can be called outside of this script
 
 class fc_lodestone_data():
-
-    formed_date = datetime.datetime.fromtimestamp(formedDate) # FC Formed At
-    name = fcName[0] # FC Name
-    tag = fcName[2] # FC Tag
-    fc_formed = f"{formed_date:%Y-%m-%d}" # Convert to readable date
+    formed_date = datetime.datetime.fromtimestamp(formedDate)
+    name = fcName[0]
+    tag = fcName[2]
+    fc_formed = f"{formed_date:%Y-%m-%d}"
+    fcRank = Rank
+    activeMembers = Members
