b54722e5e377f027c03a93921b4eb29a20bd008a
johan-sorman <johan.sorman@gmail.com>
2022-07-12 20:10:00 +0200

    update

--- htmlscrape.py
+++ htmlscrape.py
@@ -1,9 +1,6 @@
-from multiprocessing.dummy import active_children
-from threading import activeCount
-import requests, json
-import base64
-import re
+import requests, re
 from bs4  import BeautifulSoup
+import datetime
 
 # The base url to the Free Company page on Lodestone
 url = "https://eu.finalfantasyxiv.com/lodestone/freecompany/"
@@ -16,6 +13,21 @@ id = "9279667032196907056"
 result = requests.get(url+id)
 
 doc = BeautifulSoup(result.text, "html.parser")
-lst = doc.find_all("p", {"class": "freecompany__text"}) 
-for Rank in lst[4]:
-    print(Rank)
+lst2 = doc.find_all("p", {"class": "freecompany__text__name"}) 
+
+l = str(lst2)
+fcName = re.findall(r"(?<=>)[^<]+",l)
+
+timestamp = fcName[9]
+formed = re.findall(r"\b\d+\b", timestamp)
+for formed in formed:
+    formedDate = formed
+
+    formedDate = int(formedDate)
+
+class fc_lodestone_data():
+
+    formed_date = datetime.datetime.fromtimestamp(formedDate)
+    name = fcName[0]
+    tag = fcName[2]
+    fc_formed = f"{formed_date:%Y-%m-%d}"
